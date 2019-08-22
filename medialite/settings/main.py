fc6c96ee1db544fc821847ab51c9b81b2ad0004e
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-21 20:29:33 -0400

    Setup STATIC_ROOT to be outside project dir in both dev and prod

--- medialite/settings/main.py
+++ medialite/settings/main.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manager',
+    'publisher',
 ]
 
 MIDDLEWARE = [
@@ -113,3 +114,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../../static/')
