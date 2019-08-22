295852adb2f20c9d99f5a0c8f974f4724fca3e9a
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-21 23:32:30 -0400

    API Serializers for the 'Manager' app created

--- manager/api/serializers.py
+++ manager/api/serializers.py
@@ -1,17 +1,59 @@
+from django.contrib.auth.models import User 
 from rest_framework import serializers 
 from manager.models import (Editor, Writer)
 from publisher.models import (Article)
 
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the django.contrib.auth.models.User model.
+    """
+    writer = serializers.HyperlinkedModelSerializer(
+        queryset = Writer.objects.all(),
+        view_name = 'manager_api:writer_detail',
+        lookup_field = "link"
+    )
+    editor = serializers.HyperlinkedModelSerializer(
+        queryset = Editor.objects.all(),
+        view_name = 'manager_api:editor_detail',
+        lookup_field = "link"
+    )
+    class Meta:
+        model = User 
+        fields = ['first_name','last_name','pk']
+
 class EditorSerializer(serializers.HyperlinkedModelSerializer):
-    writers = None 
+    """
+    Serializer for the manager.Editor model. Returns JSON by default.
+    """
+    user = serializers.HyperlinkedRelatedField(
+        queryset = User.objects.all(),
+        view_name = 'manager_api:user_detail',
+        lookup_field = "pk"
+    )
+    writers = serializers.HyperlinkedModelSerializer(
+        queryset = Writer.objects.all(),
+        view_name = 'manager_api:writer_detail',
+        many = True
+    )
+
     class Meta:
         model = Editor 
-        fields = ['']
+        fields = ['user','link','about','date_reg']
 
 class WriterSerializer(serializer.HyperlinkedModelSerializer):
+    """
+    API Serializer for the manager.Writer model. Returns JSON format by default.
+    """
+    user = serializers.HyperlinkedRelatedField(
+        queryset = User.objects.all(),
+        view_name = "manager_api:user_detail",
+        lookup_field = "pk"
+    )
+    editor = serializers.HyperlinkedRelatedField(
+        queryset = Editor.objects.all(),
+        view_name = "manager_api:editor_detail",
+        lookup_field = "link"
+    )
     class Meta:
         model = Writer 
-        fields = ['']
-
-
-
+        fields = ['user','editor','link','about','date_reg',]
