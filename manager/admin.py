62d478e425274a5f1abe120b2a445eadf8a9cecd
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-21 22:58:48 -0400

    Register 'Manager' app's models with AdminSite

--- manager/admin.py
+++ manager/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from manager.models import (Editor, Writer)
+
 
 # Register your models here.
+admin.site.register(Editor, admin.ModelAdmin)
+admin.site.register(Writer, admin.ModelAdmin)
