88198183398efd5bf89f6515c30ab26358df1395
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-21 23:01:33 -0400

    Registered 'Publisher' app models with AdminSite

--- publisher/admin.py
+++ publisher/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from publisher.models import (Article, Comment)
 
 # Register your models here.
+admin.site.register(Article, admin.ModelAdmin)
+admin.site.register(Comment, admin.ModelAdmin)
