7e3ff87e56e3b57289827ad7c1f6f929ce1fd1bf
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-23 19:11:46 -0400

    Created CommentApi for making CRUD requests again publisher.Comment model in backend

--- publisher/static/medialite/src/api/CommentApi.js
+++ publisher/static/medialite/src/api/CommentApi.js
@@ -4,15 +4,16 @@ class CommentApi {
     constructor(){}
 
     static getCommentsforArticle(article){
-
+        return axios.get("http://localhost:8000/api/publisher/comments/");
     }
     
     static getSingleComment(link){
+        return axios.get("http://localhost:8000/api/publisher/comments/${link}/");
 
     }
 
     static postNewComment(new_comment){
-
+        return axios.post("http://localhost:8000/api/publisher/comments/", new_comment);
     }
 
 }
