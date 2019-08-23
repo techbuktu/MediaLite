710071b33f4405a28921830a9e62e165f761eecf
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-23 00:07:00 -0400

    Created Manager component to serve as central UI for Editors and Writers

---
+++ publisher/static/medialite/src/components/manager/Manager.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Manager extends Component {
+    state = {
+        writers : [ ],
+        editors : [ ]
+    }
+
+    componentDidMount(){
+        this.getEditors();
+        this.getWriters();
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
+    getWriters(){
+
+    }
+
+    getEditors(){
+
+    }
+}
+
+export default Manager;
