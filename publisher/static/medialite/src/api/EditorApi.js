0aa877cf01c3592b2e939168816ef38cbec516d8
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-23 23:40:10 -0400

    Changed CommentApi methods to 'static' methods for use with class directly

--- publisher/static/medialite/src/api/EditorApi.js
+++ publisher/static/medialite/src/api/EditorApi.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 class EditorApi {
     constructor(){}
 
-    getAllEditors(){
+    static getAllEditors(){
         return axios.get("http://localhost:8000/api/manager/editors");
     }
 
-    getSingleEditor(editor_link){
+    static getSingleEditor(editor_link){
         return axios.get("http://localhost:8000/api/manager/editors/${editor_link}/");
     }
 
-    postNewEditor(new_editor){
+    static postNewEditor(new_editor){
         return axios.post("http://localhost:8000/api/manager/editors/", new_editor);
     }
 }
