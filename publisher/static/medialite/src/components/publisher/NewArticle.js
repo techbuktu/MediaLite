038496f0bd6cbc86494c7e56aa40dbdd8dcb2422
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-24 19:09:07 -0400

    Created NewArticle component to handle creation of such

---
+++ publisher/static/medialite/src/components/publisher/NewArticle.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class NewArticle extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <h4> Publish a News Article</h4>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+export default NewArticle;
