65f3bc0d7a806a735d0f2cc2a98380d13f2aae19
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-27 17:05:13 -0400

    Default dummy route add

--- backend/Server.js
+++ backend/Server.js
@@ -5,6 +5,10 @@ const project = express();
 
 project.use(express.json());
 
+//Default route for the project
+project.get('/', (req, res) => {
+    res.send('Welcome to the MediaLite platform!')
+})
 
 //Connect to MongoDB here
 const PORT = process.env.PORT || 5000;
