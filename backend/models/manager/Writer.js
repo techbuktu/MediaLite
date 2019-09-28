03ec9ac331c43732cb4d1ee5d97de686b4d62d0d
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-28 17:17:44 -0400

    Writer model created

--- backend/models/manager/Writer.js
+++ backend/models/manager/Writer.js
@@ -3,7 +3,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const WriterSchema = new Schema({
-
+    user: {
+        type: Map,
+        //of: String
+    },
+    about: {
+        type: String, 
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    link: {
+        type: String,
+        required: false
+    },
+    editor: {
+        type: Map,
+        of: String
+    }
 });
 
-module.exports = Writer = mongoose.model('writer', WriterSchema);
+module.exports = Writer = mongoose.model('Writer', WriterSchema);
