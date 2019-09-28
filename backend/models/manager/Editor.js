42342666bc62c914325537eb6744df6fa84c8294
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-28 16:58:04 -0400

    Editor model implemented

--- backend/models/manager/Editor.js
+++ backend/models/manager/Editor.js
@@ -2,7 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EditorSchema = new Schema({
-
+    user: {
+        type: Map,
+        //of: String
+    },
+    about: {
+        type: String, 
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    link: {
+        type: String,
+        required: false
+    }
 });
 
-module.exports = Comment = mongoose.model('editor', EditorSchema);
+module.exports = Comment = mongoose.model('Editor', EditorSchema);
