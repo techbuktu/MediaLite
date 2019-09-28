a1400d3b47eb368cf768abc73d7bb98453b2aa49
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-28 16:31:28 -0400

    Fixed mongoURI error!

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,3 @@
 module.exports = {
-    mongoURI: `mongodb+srv://muhammad:Jalloh1234@cluster0-hpcwx.mongodb.net/test?retryWrites=true&w=majority`
+    mongoURI: 'mongodb+srv://muhammad:Jalloh1234@cluster0-hpcwx.mongodb.net/test?retryWrites=true&w=majority'
 };
-
