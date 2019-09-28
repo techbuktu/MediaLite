f39dcad9b3f4be69056c4dc01e9837da3b07b4b5
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-28 16:39:33 -0400

    User model implemented!

--- backend/models/manager/User.js
+++ backend/models/manager/User.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    joinDate: {
+        type: Date,
+        default: Date.now
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = User = mongoose.model('User', UserSchema);
