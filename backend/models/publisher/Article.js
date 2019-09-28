f5678689c5f78f6df076cc6d0045d4f553ef6924
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-28 17:26:10 -0400

    Article model created

--- backend/models/publisher/Article.js
+++ backend/models/publisher/Article.js
@@ -2,7 +2,30 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-
+    title: {
+        type: String, 
+        required: true
+    },
+    link: {
+        type: String,
+        required: false
+    },
+    writer: {
+        type: Map,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    publishDate: {
+        type: Date,
+        required: true
+    },
+    publish: {
+        type: Boolean,
+        required: true
+    }
 });
 
-module.exports = Article = mongoose.model('article', ArticleSchema);
+module.exports = Article = mongoose.model('Article', ArticleSchema);
