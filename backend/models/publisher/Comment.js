66ebfcb8b8b4cfb6817d75896688c7729930c337
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-28 17:33:02 -0400

    Comment model implemented

--- backend/models/publisher/Comment.js
+++ backend/models/publisher/Comment.js
@@ -2,7 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-
+    commenter: {
+        type: Map,
+        required: true
+    },
+    article: {
+        type: Map,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    commentDate: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-module.exports = Comment = mongoose.model('comment', CommentSchema);
+module.exports = Comment = mongoose.model('Comment', CommentSchema);
