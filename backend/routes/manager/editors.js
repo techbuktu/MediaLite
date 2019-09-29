6bf0839712cc169d839b5d7f5c78fc5928fc6e71
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-29 02:27:37 -0400

    Structure of Editor API routes setup

--- backend/routes/manager/editors.js
+++ backend/routes/manager/editors.js
@@ -16,5 +16,41 @@ router.get('/', (req, res) => {
 //@desc 
 //@access 
 
+//@route GET api/managers/editrs/:id
+//@desc GET a single Editor object
+//@access Public 
+router.get('/:link', (req, res) => {
+    Editor.find({ _id: req.params.link })
+        .then(editor) => {
+            res.status(200).json({editor: editor})
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `Editor with link of ${req.params.link} does not exist.`
+            })
+        })
+}); 
+
+//@route POST api/manager/editrs
+//@desc Create a New Editor object
+//@access Public
+router.post('/', (req, res) => {
+
+})
+
+//@route PUT api/manager/editrs/:id
+//@desc Update an existing Editor object
+//@access Private 
+router.put('/:id', (req, res) => {
+
+})
+
+//@route DELETE api/managers/:id
+//@desc Delete a single Editor object
+//@access Private 
+router.delete('/:id', (req, res) => {
+    
+})
+
 
 module.exports = router;
