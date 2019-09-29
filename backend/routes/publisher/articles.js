f3d08a51d3f44d57a0e1cd6d2582a96f19f7978a
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-29 02:38:01 -0400

    Structure of Publisher (Article and Comment) API setup

--- backend/routes/publisher/articles.js
+++ backend/routes/publisher/articles.js
@@ -16,6 +16,41 @@ router.get('/', (req, res) => {
 //@desc 
 //@access 
 
+//@route GET api/managers/writers/:link
+//@desc GET a single Article object
+//@access Public 
+router.get('/:link', (req, res) => {
+    User.find({ _id: req.params.link })
+        .then(user => {
+            res.status(200).json({user: user})
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `User with id of ${req.params.link} does not exist.`
+            })
+        })
+}); 
+
+//@route POST api/publisher/articles
+//@desc Create a New Article object
+//@access Public
+router.post('/', (req, res) => {
+
+})
+
+//@route PUT api/publisher/articles/:link
+//@desc Update an existing Article object
+//@access Private 
+router.put('/:link', (req, res) => {
+
+})
+
+//@route DELETE api/managers/:link
+//@desc Delete a single Article object
+//@access Private 
+router.delete('/:link', (req, res) => {
+    
+})
 
 
 
--- backend/routes/publisher/comments.js
+++ backend/routes/publisher/comments.js
@@ -16,7 +16,40 @@ router.get('/', (req, res) => {
 //@desc 
 //@access 
 
+//@route GET api/managers/writers/:link
+//@desc GET a single Comment object
+//@access Public 
+router.get('/:id', (req, res) => {
+    User.find({ _id: req.params.id })
+        .then(user => {
+            res.status(200).json({comment: comment})
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `User with id of ${req.params.id} does not exist.`
+            })
+        })
+}); 
 
+//@route POST api/publisher/Comments
+//@desc Create a New Comment object
+//@access Public
+router.post('/', (req, res) => {
 
+})
+
+//@route PUT api/publisher/Comments/:link
+//@desc Update an existing Comment object
+//@access Private 
+router.put('/:link', (req, res) => {
+
+})
+
+//@route DELETE api/managers/:link
+//@desc Delete a single Comment object
+//@access Private 
+router.delete('/:link', (req, res) => {
+    
+})
 
 module.exports = router;
