26b75d3142c13380b2f4f5a1e49e08b98f91b618
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-29 02:31:36 -0400

    Writer API routes structure setup

--- backend/routes/manager/writers.js
+++ backend/routes/manager/writers.js
@@ -16,6 +16,41 @@ router.get('/', (req, res) => {
 //@desc 
 //@access 
 
+//@route GET api/managers/writers/:link
+//@desc GET a single Writer object
+//@access Public 
+router.get('/:link', (req, res) => {
+    User.find({ _id: req.params.link })
+        .then(user => {
+            res.status(200).json({user: user})
+        })
+        .catch(err => {
+            res.status(400).json({
+                message: `User with id of ${req.params.link} does not exist.`
+            })
+        })
+}); 
+
+//@route POST api/manager/writers
+//@desc Create a New Writer object
+//@access Public
+router.post('/', (req, res) => {
+
+})
+
+//@route PUT api/manager/writers/:link
+//@desc Update an existing Writer object
+//@access Private 
+router.put('/:link', (req, res) => {
+
+})
+
+//@route DELETE api/managers/:link
+//@desc Delete a single Writer object
+//@access Private 
+router.delete('/:link', (req, res) => {
+    
+})
 
 
 
