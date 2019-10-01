b73bd94f6e5ef9592c235a71d19084a5feb4946f
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-01 19:27:30 -0400

    GET and POST /api/manager/writers: implemented

--- backend/routes/manager/writers.js
+++ backend/routes/manager/writers.js
@@ -2,14 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 //Import the 'Writer' model
-const User = require("../../models/manager/Writer");
+const Writer = require("../../models/manager/Writer");
 
 
 //@route GET api/manager/writers
 //@desc Get list of All Writer
 //@access Public 
 router.get('/', (req, res) => {
-    res.send('Home of Writers API');
+    Writer.find()
+        .then(writers => {
+            res.json({
+                successMessage: `${writers.length} writer(s) were found.`,
+                writers
+
+            })
+        })
+        .catch(apiError => {
+            res.status(400).json({
+                errorMessage: "Sorry, no writers were found"
+            })
+        })
 })
 
 //@route 
