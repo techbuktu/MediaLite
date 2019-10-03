7baccfe2ae82b4600d6bcb9105d356bea4973f76
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-03 16:54:03 -0500

    GET && POST /api/publisher/comments sucessfully-implemented!

--- backend/routes/publisher/comments.js
+++ backend/routes/publisher/comments.js
@@ -23,21 +23,24 @@ router.get('/', (req, res) => {
         })
 })
 
-//@route 
-//@desc 
-//@access 
+//@route GET api/manager/comments/:link
+//@desc GET all comments for an Article (/:link)
+//@access Private 
+router.get('/:link', (req, res) => {
 
-//@route GET api/managers/writers/:link
+})
+
+//@route GET api/managers/comments/:id
 //@desc GET a single Comment object
 //@access Public 
 router.get('/:id', (req, res) => {
-    User.find({ _id: req.params.id })
-        .then(user => {
+    Comment.find({ _id: req.params.id })
+        .then(comment => {
             res.status(200).json({comment: comment})
         })
         .catch(err => {
             res.status(400).json({
-                message: `User with id of ${req.params.id} does not exist.`
+                message: `Comment with id of ${req.params.id} does not exist.`
             })
         })
 }); 
@@ -46,7 +49,47 @@ router.get('/:id', (req, res) => {
 //@desc Create a New Comment object
 //@access Public
 router.post('/', (req, res) => {
+    //Get all submitted fields 
+    const {commenter, commentBody, article} = req.body;
 
+    if(!commenter || !commentBody || !article){
+        res.status(400).json({
+            errorMessage: "Please, supply all required fields and try again."
+        })
+    }else {
+
+        // Check if a duplicateComment exists 
+        Comment.findOne({ article: article, commentBody: commentBody})
+            .then(duplicateComment => {
+                if(duplicateComment){
+                    res.status(400).json({
+                        errorMessage: `This Comment (${duplicateComment._id}) already exists.`,
+                        duplicate_comment: duplicateComment
+                    })
+                }else {
+                    //Instantiate and save new Comment 
+                    const newComment = new Comment({
+                        commenter, commentBody, article
+                    })
+
+                    newComment.save()
+                        .then(new_comment => {
+                            res.json({
+                                successMessage: `You have sucessfully-created a new Comment(${new_comment._id})`,
+                                new_comment
+                            })
+                        })
+                        .catch(DBError => {
+                            //Return a DBError if newComment could not be saved to 'comments' collection.
+                            res.status(500).json({
+                                errorMessage: `Sorry, this comment could not be saved. Please, check your data and try again.`,
+                                DBError : DBError
+                            })
+                        })
+                }
+            })
+        
+    }
 })
 
 //@route PUT api/publisher/Comments/:link
