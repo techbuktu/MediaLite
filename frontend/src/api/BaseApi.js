bc336f87640c5970a3fee8489418c62ddd2e1368
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-05 16:07:47 -0500

    Created the custom/base Axios object for making API calls to the backend

--- frontend/src/api/BaseApi.js
+++ frontend/src/api/BaseApi.js
@@ -1,1 +1,18 @@
 import axios from 'axios';
+
+
+const auth_token = localStorage.getItem('auth_token');
+
+const RequestHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'content-type': 'application/json',
+    'x-auth-token': `${auth_token}`
+}
+
+
+const Axios = axios.create({
+    baseURL: 'http://localhost:8000/api',
+    headers: RequestHeaders,
+})
+
+export default Axios;
