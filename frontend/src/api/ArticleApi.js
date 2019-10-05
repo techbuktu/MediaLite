ade2540221c8776bb8c294d40cd42ff2ea45e520
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-05 16:50:09 -0500

    ArticleApi => implemented!

---
+++ frontend/src/api/ArticleApi.js
@@ -0,0 +1,26 @@
+import Axios from './BaseApi';
+
+class ArticleApi {
+    //Handles all API calls for the Article API endpoint: /api/publisher/articles 
+
+    static newArticle(new_article_obj){
+        return Axios.post(`/publisher/articles`, new_article_obj);
+    }
+
+    static getArticle(article_link){
+        return Axios.get(`/publisher/articles/${article_link}`);
+    }
+
+    static getAllArticles(){
+        return Axios.get(`/publisher/articles`);
+    }
+
+    static updateArticle(article_link, updated_article_obj){
+        return Axios.put(`/publisher/articles/${article_link}`, updated_article_obj);
+    }
+
+    static deleteArticle(article_link){
+        return Axios.delete(`/publisher/articles/${article_link}`);
+    }
+    
+}
