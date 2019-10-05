63d994e35668aa1946dbbd3eb9af4e65a2d475b9
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-05 16:25:14 -0500

    UserApi: Implemented

---
+++ frontend/src/api/UserApi.js
@@ -0,0 +1,29 @@
+import Axios from './BaseApi';
+
+class UserApi {
+    //Performs all CRUD ops against backend User API: /api/manager/users
+
+    static newUser(new_user_obj){
+        return Axios.post(`/manager/users`, new_user_obj);
+    }
+
+    static getUser(userId){
+        return Axios.get(`/manager/users/${userId}`);
+    }
+
+    static updateUser(userId, updatedUserObj){
+        return Axios.put(`/manager/users/${userId}`, updatedUserObj);
+    }
+
+    static deleteUser(userId){
+        return Axios.delete(`/manager/users/${userId}`);
+    }
+
+    static getAllUsers(){
+        return Axios.get(`/manager/users`);
+    }
+
+    static Login(login_creds){
+        return Axios.post(`/manager/users/login`, login_creds);
+    }
+}
