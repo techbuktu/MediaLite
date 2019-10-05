fe7c3fc8914c857903f79de8cfa9ff24cd8f0862
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-05 16:52:57 -0500

    CommentApi : structured

---
+++ frontend/src/api/CommentApi.js
@@ -0,0 +1,25 @@
+import Axios from './BaseApi';
+
+class CommentApi{
+    //For handling all CRUD ops against the Comment API endpoint: /api/publisher/comments
+
+    static newComment(){
+
+    }
+
+    static updateComment(){
+
+    }
+
+    static deleteComment(){
+
+    }
+
+    static getCommentforArticle(){
+
+    }
+
+    static getAllComments(){
+        
+    }
+}
