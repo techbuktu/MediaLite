9deb5845b7dc390056c71bc02b63c4f6dddc7d92
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-06 00:28:58 -0500

    CommentApi: Done~

--- frontend/src/api/CommentApi.js
+++ frontend/src/api/CommentApi.js
@@ -3,23 +3,23 @@ import Axios from './BaseApi';
 class CommentApi{
     //For handling all CRUD ops against the Comment API endpoint: /api/publisher/comments
 
-    static newComment(){
-
+    static newComment(new_comment_obj){
+        return Axios.post(`/publisher/comments`, new_comment_obj);
     }
 
-    static updateComment(){
-
+    static updateComment(comment_id, updated_comment_obj){
+        return Axios.put(`/publisher/comments/${comment_id}`, updated_comment_obj);
     }
 
-    static deleteComment(){
-
+    static deleteComment(comment_id){
+        return Axios.delete(`/publisher/comments/${comment_id}`);
     }
 
-    static getCommentforArticle(){
-
+    static getCommentsforArticle(article_link){
+        return Axios.get(`/publisher/comments/for/${article_link}`);
     }
 
     static getAllComments(){
-        
+        return Axios.get(`/publisher/comments/`);
     }
 }
