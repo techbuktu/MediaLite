76e4b51f27f26916116469eafd981c563848c48b
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-08 19:38:31 -0500

    Writer-related ACTION CREATORS: Implemented => actions/writerActions.js

--- frontend/src/dataStore/actions/writerActions.js
+++ frontend/src/dataStore/actions/writerActions.js
@@ -44,29 +44,87 @@ export const getWriter = (dispatch, writer_id) => {
         })
 };
 
-export const getWriterByUser = (dispatch, related_user_obj) => {
+export const getWriterByUser = (dispatch, user_id) => {
     //Handles action types: GET_WRITER_BY_USER and GET_WRITER_BY_USER_FAILED
-    WriterApi.
+    WriterApi.getWriterByUser(user_id)
+        .then(res => {
+            dispatch({
+                type: GET_WRITER_BY_USER,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_WRITER_BY_USER_FAILED,
+                payload: err
+            })
+        })
 }
 
 export const getAllWriters = (dispatch) => {
     //Handles action types: GET_ALL_WRITERS and GET_ALL_WRITERS_FAILED
-    WriterApi;
+    WriterApi.getAllWriters()
+        .then(res => {
+            dispatch({
+                type: GET_ALL_WRITERS,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_ALL_WRITERS_FAILED,
+                payload: err
+            })
+        })
 }
 
 export const updateWriter = (dispatch, writer_id, updated_writer_obj) => {
     //Handles action types: UPDATE_WRITER and UPDATE_WRITER_FAILED 
-    WriterApi;
+    WriterApi.updateWriter(writer_id, updated_writer_obj)
+        .then(res => {
+            dispatch({
+                type: UPDATE_WRITER,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: UPDATE_WRITER_FAILED,
+                payload: err
+            })
+        })
 }
 
 export const deleteWriter = (dispatch, writer_id) => {
     //Handles action types: DELETE_WRITER and DELETE_WRITER_FAILED
-    WriterApi;
+    WriterApi.deleteWriter(writer_id)
+        .then(res => {
+            dispatch({
+                type: DELETE_WRITER,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: DELETE_WRITER_FAILED ,
+                payload: err
+            })
+        })
 }
 
 export const getWritersByEditor = (dispatch, editor_id) =>{
     //Handles action types: GET_WRITERS_BY_EDITOR and GET_WRITERS_BY_EDITOR_FAILED
     WriterApi.getWritersByEditor(editor_id)
-        .then(res => {})
-        .catch(err => {})
+        .then(res => {
+            dispatch({
+                type: GET_WRITERS_BY_EDITOR,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_WRITERS_BY_EDITOR_FAILED,
+                payload: err
+            })
+        })
 }
