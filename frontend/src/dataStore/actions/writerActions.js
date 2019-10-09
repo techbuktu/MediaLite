9f79bf7225c3e55a87e28c3c7c5d65039d76bbdf
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-08 19:27:06 -0500

    Added new Writer-related actionTYPES

--- frontend/src/dataStore/actions/writerActions.js
+++ frontend/src/dataStore/actions/writerActions.js
@@ -3,7 +3,7 @@ import WriterApi from '../../api/WriterApi';
 
 import { NEW_WRITER, NEW_WRITER_FAILED, GET_WRITER, GET_WRITER_BY_USER, GET_WRITER_BY_USER_FAILED,
     GET_WRITER_FAILED, GET_ALL_WRITERS,GET_ALL_WRITERS_FAILED, UPDATE_WRITER, UPDATE_WRITER_FAILED, 
-    DELETE_WRITER, DELETE_WRITER_FAILED
+    DELETE_WRITER, DELETE_WRITER_FAILED, GET_WRITERS_BY_EDITOR, GET_WRITERS_BY_EDITOR_FAILED
 
 } from './types/writers';
 //One 'exported' action creator for each action type 
@@ -11,17 +11,42 @@ import { NEW_WRITER, NEW_WRITER_FAILED, GET_WRITER, GET_WRITER_BY_USER, GET_WRIT
 
 export const newWriter = (dispatch, new_writer_obj) => {
     //Handles action types: NEW_WRITER and NEW_WRITER_FAILED
-    WriterApi;
-}
+    WriterApi.newWriter(new_writer_obj)
+        .then(res => {
+            dispatch({
+                type: NEW_WRITER,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: NEW_WRITER_FAILED,
+                payload: err
+            })
+        })
+};
+
 
 export const getWriter = (dispatch, writer_id) => {
     //Handles action types: GET_WRITER, GET_WRITER_FAILED
-    WriterApi;
-}
+    WriterApi.getWriter(writer_id)
+        .then(res => {
+            dispatch({
+                type: GET_WRITER,
+                payload: res.data
+            })
+        })
+        .catch(err => {
+            dispatch({
+                type: GET_WRITER_FAILED,
+                payload: err
+            })
+        })
+};
 
 export const getWriterByUser = (dispatch, related_user_obj) => {
     //Handles action types: GET_WRITER_BY_USER and GET_WRITER_BY_USER_FAILED
-    WriterApi;
+    WriterApi.
 }
 
 export const getAllWriters = (dispatch) => {
@@ -37,4 +62,11 @@ export const updateWriter = (dispatch, writer_id, updated_writer_obj) => {
 export const deleteWriter = (dispatch, writer_id) => {
     //Handles action types: DELETE_WRITER and DELETE_WRITER_FAILED
     WriterApi;
+}
+
+export const getWritersByEditor = (dispatch, editor_id) =>{
+    //Handles action types: GET_WRITERS_BY_EDITOR and GET_WRITERS_BY_EDITOR_FAILED
+    WriterApi.getWritersByEditor(editor_id)
+        .then(res => {})
+        .catch(err => {})
 }
