6c0b39266b0edb8fc52e861ac887527d47a1df38
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-08 19:18:58 -0500

    Setup structure for Article-related ACTION CREATORS

--- frontend/src/dataStore/actions/articleActions.js
+++ frontend/src/dataStore/actions/articleActions.js
@@ -1,15 +1,30 @@
 // Import all action types for the Article 
 import ArticleApi from '../../api/ArticleApi';
 
+//Import all Article-related action TYPES
+import { NEW_ARTICLE, NEW_ARTICLE_FAILED, GET_ARTICLE, GET_ARTICLE_FAILED, GET_ALL_ARTICLES,
+    GET_ALL_ARTICLES_FAILED, UPDATE_ARTICLE, UPDATE_ARTICLE_FAILED, DELETE_ARTICLE, DELETE_ARTICLE_FAILED
+} from './types/articles';
+
 //One 'exported' action creator for each action type 
 //(Error-related action creators to be dispatch()ed inside their appropriate .catch() callbacks
-export const NEW_ARTICLE = "NEW_ARTICLE"
-export const NEW_ARTICLE_FAILED = "NEW_ARTICLE_FAILED"
-export const GET_ARTICLE = "GET_ARTICLE"
-export const GET_ARTICLE_FAILED = "GET_ARTICLE_FAILED"
-export const GET_ALL_ARTICLES = "GET_ALL_ARTICLES"
-export const GET_ALL_ARTICLES_FAILED = "GET_ALL_ARTICLES_FAILED"
-export const UPDATE_ARTICLE = "UPDATE_ARTICLE"
-export const UPDATE_ARTICLE_FAILED = "UPDATED_ARTICLE_FAILED"
-export const DELETE_ARTICLE = "DELETE_ARTICLE"
-export const DELETE_ARTICLE_FAILED = "DELETE_ARTICLE_FAILED"
+
+export const newArticle = (dispatch, new_article_obj) => {
+    //Handles action types: NEW_ARTICLE, NEW_ARTICLE_FAILED
+    ArticleApi;
+}
+
+export const getArticle = (dispatch, article_id) => {
+    //Handles action types: GET_ARTICLE, GET_ARTICLE_FAILED
+    ArticleApi;
+}
+
+export const getALlArticles = (dispatch) => {
+    //Handles action types: GET_ALL_ARTICLES, GET_ALL_ARTICLES_FAILED
+    ArticleApi;
+}
+
+export const updateArticle = (dispatch, article_id, updated_article_obj) => {
+    //Handles action types: UPDATE_ARTICLE, UPDATE_ARTICLE_FAILED
+    ArticleApi;
+}
