5decb6c13b374b0a4b6df9f5a4db92b432675a6a
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-08 19:05:33 -0500

    Laid out structure for writer-related ACTION CREATORS => actions/writerActions.js

--- frontend/src/dataStore/actions/writerActions.js
+++ frontend/src/dataStore/actions/writerActions.js
@@ -8,3 +8,33 @@ import { NEW_WRITER, NEW_WRITER_FAILED, GET_WRITER, GET_WRITER_BY_USER, GET_WRIT
 } from './types/writers';
 //One 'exported' action creator for each action type 
 //(Error-related action creators to be dispatch()ed inside their appropriate .catch() callbacks
+
+export const newWriter = (dispatch, new_writer_obj) => {
+    //Handles action types: NEW_WRITER and NEW_WRITER_FAILED
+    WriterApi;
+}
+
+export const getWriter = (dispatch, writer_id) => {
+    //Handles action types: GET_WRITER, GET_WRITER_FAILED
+    WriterApi;
+}
+
+export const getWriterByUser = (dispatch, related_user_obj) => {
+    //Handles action types: GET_WRITER_BY_USER and GET_WRITER_BY_USER_FAILED
+    WriterApi;
+}
+
+export const getAllWriters = (dispatch) => {
+    //Handles action types: GET_ALL_WRITERS and GET_ALL_WRITERS_FAILED
+    WriterApi;
+}
+
+export const updateWriter = (dispatch, writer_id, updated_writer_obj) => {
+    //Handles action types: UPDATE_WRITER and UPDATE_WRITER_FAILED 
+    WriterApi;
+}
+
+export const deleteWriter = (dispatch, writer_id) => {
+    //Handles action types: DELETE_WRITER and DELETE_WRITER_FAILED
+    WriterApi;
+}
