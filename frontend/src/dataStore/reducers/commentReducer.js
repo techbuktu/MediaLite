218200646196517c5f4cca42106151b7fbe9cd5c
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-11 00:10:55 -0500

    commentReducer : implemented => /reducers/commentReducer.js and added to rootReducer as 'comment'

--- frontend/src/dataStore/reducers/commentReducer.js
+++ frontend/src/dataStore/reducers/commentReducer.js
@@ -6,7 +6,7 @@ import {
 
 const initialState = {
     comment_list: [],
-    comments_for_article: [],
+    comments_for_article: [], // May also be implemented/function the same as comment_list???
     comment: {},
     updated_comment: {},
     errorMessage: {},
@@ -14,8 +14,75 @@ const initialState = {
 };
 
 commentReducer = (state=initialState, action) => {
-    
-}
+    switch(action.type){
+        case NEW_COMMENT:
+            return {
+                ...state, 
+                comment: action.payload.new_comment, // .comment : VERIFY
+                comment_list : [...comment_list, action.payload.new_comment]
+            };
+        case NEW_COMMENT_FAILED:
+            return {
+                ...state,
+                errorMessage: action.payload.errorMessage,
+                //comment_error: ???
+            };
+        case GET_COMMENT:
+            return {
+                ...state, 
+                comment: action.payload.comment
+            };
+        case GET_COMMENT_FAILED:
+            return {
+                ...state, 
+                errorMessage: action.payload.errorMessage,
+                //comment_error: ???
+            };
+        case UPDATE_COMMENT:
+            return {
+                ...state,
+                comment: action.payload.updated_comment,
+            };
+        case UPDATE_COMMENT_FAILED:
+            return {
+                ...state, 
+                errorMessage: action.payload.errorMessage
+            };
+        case DELETE_COMMENT:
+            return {
+                ...state, 
+                comment_list: [] // -minus action.payload.deleted_comment 
+            };
+        case DELETE_COMMENT_FAILED:
+            return {
+                ...state,
+                errorMessage: action.payload.errorMessage
+            };
+        case GET_ALL_COMMENTS:
+            return {
+                ...state, 
+                comment_list: action.payload.comment_list
+            };
+        case GET_ALL_COMMENTS_FAILED:
+            return {
+                ...state,
+                errorMessage: action.payload.errorMessage
+            };
+        case GET_COMMENTS_FOR_ARTICLE:
+            return {
+                ...state,
+                comment_list: action.payload.comment_list,
+                comments_for_article: action.payload.comment_list //May be redundant
+            };
+        case GET_COMMENTS_FOR_ARTICLE_FAILED:
+            return {
+                ...state,
+                errorMessage: action.payload.errorMessage
+            };
+        default:
+            return state
+    }
+};
 
 
-
+export default commentReducer;
--- frontend/src/dataStore/reducers/index.js
+++ frontend/src/dataStore/reducers/index.js
@@ -4,13 +4,14 @@ import userReducer from './userReducer';
 import editorReducer from './editorReducer';
 import writerReducer from './writerReducer';
 import articleReducer from './articleReducer';
+import commentReducer from './commentReducer';
 
 const rootReducer = combineReducers({
     user: userReducer,
     editor: editorReducer,
     writer: writerReducer,
     article: articleReducer,
-    comments: {}
+    comment: commentReducer
 });
 
 export default rootReducer;
