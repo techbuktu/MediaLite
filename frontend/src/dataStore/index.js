5fe47d845ebcd856db07c309d8cee7c2de8e3160
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-11 16:50:38 -0500

    Fixed REDUX_DEVTOOLS_EXTTENSION config in 'store' object instantiation, using latest RDT extension's official setting

--- frontend/src/dataStore/index.js
+++ frontend/src/dataStore/index.js
@@ -1,15 +1,19 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = { };
 const middleware = [thunk];
 
+//REDUX_DEVTOOLS_EXTENSION Setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     rootReducer,
-    initialState, 
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS__EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__( )
+    initialState,
+    composeEnhancers(
+        applyMiddleware(...middleware)
+    )
 );
 
 export default store;
