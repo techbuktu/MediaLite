b51694ed6681fbd396136d60174b3128751cac85
Muhammad Jalloh <dev@TechBuktu.com>
2019-10-21 17:06:55 -0500

    Installed jsonwebtoken and config packages; added 'jwtSecret' to config

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://muhammad:Jalloh1234@cluster0-hpcwx.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://muhammad:Jalloh1234@cluster0-hpcwx.mongodb.net/test?retryWrites=true&w=majority',
+    jwtSecret: 'sooperS3cretforJWT'
 };
