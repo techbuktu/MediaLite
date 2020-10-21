1f362970021f9f2fc9e96ac2f883048d76db4a39
Muhammad Jalloh <dev@TechBuktu.com>
2020-10-21 17:30:18 -0500

    Implemented app-level ContextAPI-based 'AppContext' and 'AppProvider' objects

--- frontend/src/contextState/index.js
+++ frontend/src/contextState/index.js
@@ -1,3 +1,43 @@
+import React, { createContext, useReducer} from 'react';
+
+//Import the individual reducers for each API resource/object type
+import AppReducer from './reducers'
+
+//REDUX_DEVTOOLS_EXTENSION Setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialAppState = {
+
+}
+
+export const AppContext = createContext(AppReducer, initialAppState);
+
+// Create the 'Provider' to wrap the components of the app
+// to allow flow of app-level state to all children components
+
+export const AppProvider = (props) => {
+    const [state, dispatch] = useReducer(AppReducer, initialAppState);
+    
+    return (
+        <AppContext.Provider value={{
+            users: state.users,
+            editors: state.editors,
+            writers: state.writers,
+            articles: state.articles,
+            comments: state.comments
+        }}>
+            { props.children }
+
+        </AppContext.Provider>
+    )
+
+}
+
+
+
+
+
+/**
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
@@ -17,3 +57,5 @@ const store = createStore(
 );
 
 export default store;
+
+*/
