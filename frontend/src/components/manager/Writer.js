b327e56b5fcf9fb68c1edfbc96b5c1b69e03c163
Muhammad Jalloh <dev@TechBuktu.com>
2020-10-31 15:23:49 -0500

    Re-implemented manager/Writer component using hooks and the Context API

--- frontend/src/components/manager/Writer.js
+++ frontend/src/components/manager/Writer.js
@@ -1,45 +1,50 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect } from 'react';
 //import react-router-dom components
-import { Redirect, Link } from 'react-router-dom'; 
+import { Redirect, Link, useParams } from 'react-router-dom'; 
 //import action creators
 import { getWriter } from '../../contextState/actions/writerActions'; //getAllArticlesforWriter() too
-import { getAllArticlesByWriter } from '../../contextState/actions/articleActions';
+import { getAllArticles, getAllArticlesByWriter } from '../../contextState/actions/articleActions';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { AppContext } from '../../contextState';
 
-class Writer extends Component {
-    componentDidMount(){
-        let writerLink = this.props.match.params.writerLink;
-        this.props.getWriter(writerLink);
-        this.props.getAllArticlesByWriter(writerLink);
-    };
 
-    render() {
-        const articlesByWriterUI = this.props.articles_by_writer.map(article => {
-            return (
-                <li key={article.link}>
-                    <Link to={`/articles/${article.link}`}> {article.title} </Link>
-                </li>
-            )
-        })
-        if(this.props.writer.user){
-            const {about, user} = this.props.writer;
-            return (
-                <div>
-                   <h4> {user.firstName} {user.lastName} </h4>
-                   <p>
-                       {about}
-                   </p>
-                   <h5> {user.firstName} {user.lastName}'s Articles:</h5>
-                   <ul>
-                        {articlesByWriterUI}
-                   </ul>
-                </div>
-            )
-        }
-        else {
-            return ('Loading...writer... and his/her penmanly entourage!')
-        }
+const Writer =  () => {
+    const {writerState, writerDispatch} = useContext(AppContext);
+    const {articleState, articleDispatch} = useContext(AppContext);
+
+    let { writerLink } = useParams();
+
+    useEffect(() => {
+        getWriter(writerLink)(writerDispatch);
+        getAllArticlesByWriter(writerLink)(writerDispatch);
+
+    }, [writerState])
+
+
+    const articlesByWriterUI = articleState.article_list.map(article => {
+        return (
+            <li key={article.link}>
+                <Link to={`/articles/${article.link}`}> {article.title} </Link>
+            </li>
+        )
+    })
+    if(writerState.writer.user){
+        const {about, user} = writerState.writer;
+        return (
+            <div>
+                <h4> {user.firstName} {user.lastName} </h4>
+                <p>
+                    {about}
+                </p>
+                <h5> {user.firstName} {user.lastName}'s Articles:</h5>
+                <ul>
+                    {articlesByWriterUI}
+                </ul>
+            </div>
+        )
+    }
+    else {
+        return ('Loading...writer... and his/her penmanly entourage!')
     }
 }
 
@@ -52,12 +57,6 @@ Writer.propTypes ={
     articles_by_writer: PropTypes.array 
 };
 
-const mapStateToProps = (state) => ({
-    //add obj: state.<reducer_key>.obj_name; one for each component prop
-    writer: state.writers.writer,
-    errorMessage: state.writers.errorMessage,
-    articles_by_writer: state.articles.article_list
-});
 
 
-export default connect(mapStateToProps, { getWriter, getAllArticlesByWriter })(Writer)
+export default Writer;
